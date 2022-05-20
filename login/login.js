906c3cd326a19b32f66c7dcd4639bfe1ee1657ee
Mohammed Arham Khan <md.arhamkhan09@gmail.com>
2022-05-20 09:08:44 +0530

    Add files via upload

--- login/login.js
+++ login/login.js
@@ -1,6 +1,6 @@
 function login(form){
  if(form.logemail.value == "admin@gmail.com" && form.logpass.value == "123") {
-    window.open("https://www.youraddress.com")
+    window.open("https://arham5416.github.io/admin-demo/admin/html&css/pages/dashboard/dashboard.html")
   }
  else {
    alert("Error Password or Username")/*displays error message*/
